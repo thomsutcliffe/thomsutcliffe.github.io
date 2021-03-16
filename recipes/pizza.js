8ffa1e8b5ce2b5647182502bb2696fcf0e583ea6
t.sutcliffe <t.sutcliffe@ocado.com>
2021-03-16 19:32:42 +0000

    Add condensed render mode.

--- recipes/pizza.js
+++ recipes/pizza.js
@@ -4,38 +4,45 @@ var pizza = {
   "forkUrl": "https://www.ilovecooking.ie/food-tv/patrick-ryans-no-fuss-sourdough-pizza/",
   "ingredients": [
     {
+      "id": 0,
       "name": "Strong white flour",
       "quantity": 500,
       "unit": "g"
     },
     {
+      "id": 1,
       "name": "Semolina",
       "quantity": 50,
       "unit": "g"
     },
     {
+      "id": 2,
       "name": "Salt",
       "quantity": 10,
       "unit": "g"
     },
     {
+      "id": 3,
       "name": "Sourdough starter",
       "quantity": 200,
       "unit": "g",
       "notes": "Very active"
     },
     {
+      "id": 4,
       "name": "Yeast",
       "quantity": 1,
       "unit": "g",
       "notes": "Dried active"
     },
     {
+      "id": 5,
       "name": "Water",
       "quantity": 325,
       "unit": "ml"
     },
     {
+      "id": 6,
       "name": "Olive oil",
       "quantity": 50,
       "unit": "ml"
@@ -44,7 +51,8 @@ var pizza = {
   "steps": [
     {
       "name": "Mix",
-      "description": "Combine all ingredients and mix by hand, knead for 30 seconds."
+      "description": "Combine all ingredients and mix by hand, knead for 30 seconds.",
+      "ingredients": [0, 1, 2, 3, 4, 5, 6]
     },
     {
       "name": "Rise",
--- recipes/shortbread.js
+++ recipes/shortbread.js
@@ -4,47 +4,68 @@ var shortbread = {
   "forkUrl": "https://www.whatlarsyloves.com/2015/07/guest-post-they-called-it-diamond-blog.html",
   "ingredients": [
     {
+      "id": 0,
       "name": "Plain flour",
       "quantity": 225,
       "unit": "g"
     },
     {
+      "id": 1,
       "name": "Sugar",
-      "quantity": 275,
+      "quantity": 75,
       "unit": "g"
     },
     {
+      "id": 2,
+      "name": "Sugar",
+      "quantity": 200,
+      "unit": "g"
+    },
+    {
+      "id": 3,
       "name": "Butter",
-      "quantity": 320,
+      "quantity": 150,
       "unit": "g"
     },
     {
+      "id": 4,
+      "name": "Butter",
+      "quantity": 170,
+      "unit": "g"
+    },
+    {
+      "id": 5,
       "name": "Raspberries",
       "quantity": 200,
       "unit": "g"
     },
     {
+      "id": 6,
       "name": "Water",
       "quantity": 2,
       "unit": "tbsp"
     },
     {
+      "id": 7,
       "name": "Condensed milk",
       "quantity": 397,
       "unit": "g"
     },
     {
+      "id": 8,
       "name": "Pink food colouring",
       "quantity": 1,
       "unit": " drop",
       "optional": true
     },
     {
+      "id": 9,
       "name": "White chocolate",
       "quantity": 300,
       "unit": "g"
     },
     {
+      "id": 10,
       "name": "Ruby chocolate",
       "quantity": 50,
       "unit": "g"
@@ -53,11 +74,18 @@ var shortbread = {
   "steps": [
     {
       "name": "Make shortbread",
-      "description": "Mix 75g sugar with the flour then rub in 150g butter. Press into a 20x30cm tin and bake at 160 for 25 minutes."
+      "description": "Mix sugar and flour then rub in butter. Press into a 20x30cm tin and bake at 160 for 25 minutes.",
+      "ingredients": [0, 1, 3]
+    },
+    {
+      "name": "Make raspberry juice",
+      "description": "Mix raspberries and water in a pan, heat for 15 minutes, mash and strain out the pips.",
+      "ingredients": [5, 6]
     },
     {
       "name": "Make caramel",
-      "description": "Mix raspberries and water in a pan, heat for 15 minutes, mash and strain out the pips.\nMelt 170g butter and 200g sugar in a pan, add condensed milk, raspberry juice and optional pink food colouring then heat and stir for 10 minutes."
+      "description": "Melt butter and sugar in a pan, add condensed milk, raspberry juice and optional pink food colouring then heat and stir for 10 minutes.",
+      "ingredients": [4, 2]
     },
     {
       "name": "Assemble",
@@ -65,7 +93,8 @@ var shortbread = {
     },
     {
       "name": "Decorate",
-      "description": "Melt the white and ruby chocolates separately. Pour the white over the caramel then flick with ruby and feather horizontally and vertically."
+      "description": "Melt the white and ruby chocolates separately. Pour the white over the caramel then flick with ruby and feather horizontally and vertically.",
+      "ingredients": [9, 10]
     },
     {
       "name": "Cut",
