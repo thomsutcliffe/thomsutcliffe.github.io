e486ebea775e235d5700588e0456c505435f2fb5
t.sutcliffe <t.sutcliffe@ocado.com>
2021-03-16 18:59:55 +0000

    Recipe changes

---
+++ recipes/shortbread.js
@@ -0,0 +1,75 @@
+var shortbread = {
+  "name": "Millionaire Shortbread",
+  "forkName": "whatlarsyloves",
+  "forkUrl": "https://www.whatlarsyloves.com/2015/07/guest-post-they-called-it-diamond-blog.html",
+  "ingredients": [
+    {
+      "name": "Plain flour",
+      "quantity": 225,
+      "unit": "g"
+    },
+    {
+      "name": "Sugar",
+      "quantity": 275,
+      "unit": "g"
+    },
+    {
+      "name": "Butter",
+      "quantity": 320,
+      "unit": "g"
+    },
+    {
+      "name": "Raspberries",
+      "quantity": 200,
+      "unit": "g"
+    },
+    {
+      "name": "Water",
+      "quantity": 2,
+      "unit": "tbsp"
+    },
+    {
+      "name": "Condensed milk",
+      "quantity": 397,
+      "unit": "g"
+    },
+    {
+      "name": "Pink food colouring",
+      "quantity": 1,
+      "unit": " drop",
+      "optional": true
+    },
+    {
+      "name": "White chocolate",
+      "quantity": 300,
+      "unit": "g"
+    },
+    {
+      "name": "Ruby chocolate",
+      "quantity": 50,
+      "unit": "g"
+    }
+  ],
+  "steps": [
+    {
+      "name": "Make shortbread",
+      "description": "Mix 75g sugar with the flour then rub in 150g butter. Press into a 20x30cm tin and bake at 160 for 25 minutes."
+    },
+    {
+      "name": "Make caramel",
+      "description": "Mix raspberries and water in a pan, heat for 15 minutes, mash and strain out the pips.\nMelt 170g butter and 200g sugar in a pan, add condensed milk, raspberry juice and optional pink food colouring then heat and stir for 10 minutes."
+    },
+    {
+      "name": "Assemble",
+      "description": "Once caramel has cooled slightly, pour it over the shortbread."
+    },
+    {
+      "name": "Decorate",
+      "description": "Melt the white and ruby chocolates separately. Pour the white over the caramel then flick with ruby and feather horizontally and vertically."
+    },
+    {
+      "name": "Cut",
+      "description": "Wait until the chocolate is just set then cut the chocolate into squares with a sharp knife. Don't cut through the caramel.\nSet the whole thing in the fridge, then cut all the way through along the same lines."
+    }
+  ]
+}
